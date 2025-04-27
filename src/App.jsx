7c66a750cc8b77b0f80c78c3a7a82ca6e2fc1d69
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-04-27 13:26:36 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,74 @@ import img from './assets/images/img-home-page.png'
 import { useState } from 'react'
 
 export default function App() {
-  const [mostrarSection, setMostrarSection] = useState(false);
+  const [mostrarDescricao, setMostrarDescricao] = useState(false);
 
-  const mostrarOutraSection = () => {
-    setMostrarSection(true);
+  const [mostrarVideo, setMostrarVideo] = useState(false);
+
+  const mostrarOutraDescricao = () => {
+    setMostrarDescricao(true);
   };
+
+  const fecharDescricao = () => {
+    setMostrarDescricao(false);
+  };
+
+  const acionarVideo = () => {
+    setMostrarVideo(true);
+  }
+
+  const fecharVideo = () => {
+    setMostrarVideo(false);
+  }
 
   return (
     <>
       <Menu name01='Home Page' name02='Personagens' />
       <section className='s1'>
         <div className='main-img'>
-          <img src={img} alt=""/>
+          <img src={img} alt="" />
         </div>
         <div className='btn-wrap-content'>
+          
           <div className='text-box'>
-          <p>Slogan dos santos do capeta junior</p>
+            <p>Slogan dos santos do capeta junior</p>
           </div>
-          <button className='btn-show-description' onClick={mostrarOutraSection}>
-           Ver Mais
+          <button className='btn-show-description' onClick={mostrarOutraDescricao}>
+            Ver Mais
           </button>
+          {mostrarDescricao && (
+          <div className='description-container'>
+            <div className='close-button'>
+              <button onClick={fecharDescricao}>Fechar</button>
+            </div>
+            <div className='description-title'>
+              <h4 style={{color: '#000', marginBottom: '50px', textAlign: 'center'}}>O que é Dragon Ball?</h4>
+            </div>
+            <div className='description-text'>
+              <p>Dragon Ball é uma franquia japonesa de anime e mangá criada por Akira Toriyama,
+                conhecida por sua história de lutas, personagens icônicos e humor. O mangá foi publicado
+                pela primeira vez em 1984 e deu origem a diversas adaptações em anime, incluindo Dragon Ball,
+                Dragon Ball Z, Dragon Ball GT e Dragon Ball Super. </p>
+            </div>
+            <div className='video-btn-show'>
+              <button onClick={acionarVideo}>Ver video</button>
+            </div>
+          </div>
+        )}
         </div>
       </section>
-      { mostrarSection && (
+      { mostrarVideo && (
         <section className='s2'>
-        <div className='close-button'></div>
-        <div className='description-title'></div>
-        <div className='description-text'>
-          <p>Dragon Ball é uma franquia japonesa de anime e mangá criada por Akira Toriyama, 
-            conhecida por sua história de lutas, personagens icônicos e humor. O mangá foi publicado 
-            pela primeira vez em 1984 e deu origem a diversas adaptações em anime, incluindo Dragon Ball, 
-            Dragon Ball Z, Dragon Ball GT e Dragon Ball Super. </p>
+        <div>
+          <div className='btn-close-video'>
+            <button onClick={fecharVideo}>Fechar</button>
+          </div>
+          <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/brrEa9K0j2k?si=15KzehACwLi0h3B_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </div>
-      </section>)}
+      </section>
+      )}
     </>
   )
 }
